d8dac8d1cae0bad58eb3b5c402f319e36705c796
BUsuper <merzlyakov96@gmail.com>
2024-12-03 15:34:30 +1000

    Put some helper functions into a separate module
    As both BigWeatherBox and SmallWeather box used 3 functions that were exactly the same, I removed them from the components and added a separate js file that includes the helper functions

--- weather-project/src/BigWeatherBox.jsx
+++ weather-project/src/BigWeatherBox.jsx
@@ -2,12 +2,8 @@ import { forwardRef } from 'react'
 import './BigWeatherBox.css'
 import TinyWeatherBox from './TinyWeatherBox'
 import locationArrow from './assets/location-arrow.svg'
-import sunnyIcon from './assets/sunny.svg'
-import moonIcon from './assets/night.svg'
-import cloudyIcon from './assets/cloudy.svg'
-import rainyIcon from './assets/rain.svg'
-import snowyIcon from './assets/snow.svg'
 import trashIcon from './assets/trash.svg'
+import { convertWindDirection, displayPrecipitationProbability, displayIcon } from './weatherfunctions'
 
 const BigWeatherBox = forwardRef(
 function BigWeatherBox(props, ref) {
@@ -32,57 +28,6 @@ function BigWeatherBox(props, ref) {
   const { temperature_2m_min: minTemps,
           temperature_2m_max: maxTemps,
   } = daily;
-
-  function convertWindDirection (degrees) {
-      const directions = ['N', 'NNE', 'NE', 'ENE', 
-                          'E', 'ESE', 'SE', 'SSE', 
-                          'S', 'SSW', 'SW', 'WSW', 
-                          'W', 'WNW', 'NW', 'NNW'];
-
-      const position = Math.floor(((degrees + 11.25) % 360) / 22.5)
-      return directions[position];
-  }
-
-  function displayPrecipitationProbability (probability, snow, rain, showers, temperature) {
-      let type;
-      let largest = 0;
-  
-      const precipitation = {
-        'Snow': snow,
-        'Rain': rain,
-        'Showers': showers,
-      };
-  
-      for (const property in precipitation) {
-        if (precipitation[property] > largest) {
-          type = property;
-          largest = precipitation[property];
-        }
-      }
-
-      if (largest < 0.1) {
-          type = temperature < 0 ? 'Snow' : 'Rain';
-        }
-    
-        return `${type}: ${probability}%`;
-  }
-
-  function displayIcon (rain, showers, snow, cloudCover, isDay) {
-    if (rain >= 1 || showers >= 1) {
-      return rainyIcon;
-    }
-    if (snow >= 0.1) {
-      return snowyIcon;
-    }
-    if (cloudCover >= 25) {
-      return cloudyIcon;
-    }
-    if (isDay == 1) {
-      return sunnyIcon;
-    } else {
-      return moonIcon;
-    }
-  }
 
   const conditionsSrc = displayIcon(rain[0], showers[0], snow[0], cloudCover[0], isDay[0]);
 
--- weather-project/src/SmallWeatherBox.jsx
+++ weather-project/src/SmallWeatherBox.jsx
@@ -1,10 +1,6 @@
 import './SmallWeatherBox.css'
 import locationArrow from './assets/location-arrow.svg'
-import sunnyIcon from './assets/sunny.svg'
-import moonIcon from './assets/night.svg'
-import cloudyIcon from './assets/cloudy.svg'
-import rainyIcon from './assets/rain.svg'
-import snowyIcon from './assets/snow.svg'
+import { convertWindDirection, displayPrecipitationProbability, displayIcon } from './weatherfunctions'
 
 function SmallWeatherBox({geo=false, locationObj, weatherObj, onClick}) {
   const { country, city, lat, lon } = locationObj;
@@ -22,57 +18,6 @@ function SmallWeatherBox({geo=false, locationObj, weatherObj, onClick}) {
   const { temperature_2m: temperatureUnit,
           wind_speed_10m: windSpeedUnit,
   } = units;
-
-  function convertWindDirection (degrees) {
-      const directions = ['N', 'NNE', 'NE', 'ENE', 
-                          'E', 'ESE', 'SE', 'SSE', 
-                          'S', 'SSW', 'SW', 'WSW', 
-                          'W', 'WNW', 'NW', 'NNW'];
-
-      const position = Math.floor(((degrees + 11.25) % 360) / 22.5)
-      return directions[position];
-  }
-
-  function displayPrecipitationProbability (probability, snow, rain, showers, temperature) {
-      let type;
-      let largest = 0;
-  
-      const precipitation = {
-        'Snow': snow,
-        'Rain': rain,
-        'Showers': showers,
-      };
-  
-      for (const property in precipitation) {
-        if (precipitation[property] > largest) {
-          type = property;
-          largest = precipitation[property];
-        }
-      }
-
-      if (largest < 0.1) {
-          type = temperature < 0 ? 'Snow' : 'Rain';
-        }
-    
-        return `${type}: ${probability}%`;
-  }
-
-  function displayIcon (rain, showers, snow, cloudCover, isDay) {
-    if (rain >= 1 || showers >= 1) {
-      return rainyIcon;
-    }
-    if (snow >= 0.1) {
-      return snowyIcon;
-    }
-    if (cloudCover >= 25) {
-      return cloudyIcon;
-    }
-    if (isDay == 1) {
-      return sunnyIcon;
-    } else {
-      return moonIcon;
-    }
-  }
 
   const conditionsSrc = displayIcon(rain[0], showers[0], snow[0], cloudCover[0], isDay[0]);
 
